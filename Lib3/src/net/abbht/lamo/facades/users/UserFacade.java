86a57d3ce71c65ef5fa9d2096d4894644dd55ed9
mdshannan <mdshannan@afb23650-9424-0410-ac3c-bbcb0118fe35>
2006-12-14 23:04:54 +0000

    changes

--- Lib3/src/net/abbht/lamo/facades/users/UserFacade.java
+++ Lib3/src/net/abbht/lamo/facades/users/UserFacade.java
@@ -5,46 +5,93 @@ import java.util.List;
 import net.abbht.lamo.persistence.users.*;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import net.abbht.lamo.utils.*;
 
 /**
- * 
- * 
+ *
+ *
  * @netbeans.hibernate.facade beanClass = "net.abbht.lamo.persistence.users.User
- "
+ * "
  */
-public class UserFacade { 
+public class UserFacade {
     public void saveUser(User user) {
-        Session session = net.abbht.lamo.utils.HibernateUtil.currentSession();
-        Transaction tx = session.beginTransaction();
-        session.save(user);
-        
-        tx.commit();
-        net.abbht.lamo.utils.HibernateUtil.closeSession();
+        Session session = HibernateUtil.currentSession();
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            session.save(user);
+        } finally {
+            tx.commit();
+            HibernateUtil.closeSession();
+        }
     }
     
     public void saveOrUpdateUser(User user) {
-        Session session = net.abbht.lamo.utils.HibernateUtil.currentSession();
-        Transaction tx = session.beginTransaction();
-        session.update(user);        
-        tx.commit();
-        net.abbht.lamo.utils.HibernateUtil.closeSession();
-    }    
+        Session session = HibernateUtil.currentSession();
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            session.update(user);
+        } finally {
+            tx.commit();
+            HibernateUtil.closeSession();
+        }
+    }
     
-      public User findByPrimaryKey(Object key) {
+    public List findByLastName(String lastname) {
+        String sql = "from net.abbht.lamo.persistence.users.User u where u.lastname = '" + lastname + "'";
+        try {
+            return  HibernateUtil.currentSession().createQuery(sql).list();
+        } finally {
+            HibernateUtil.closeSession();
+        }
         
-        return this.findByPrimaryKey(key);
         
     }
-      
-       public List findByName(String lastname) {
-        String sql = "from User u where u.lastname = '" + lastname + "'";
-        return  net.abbht.lamo.utils.HibernateUtil.currentSession().createQuery(sql).list();
-       
+    public List findByFirstName(String firstname) {
+        String sql = "from net.abbht.lamo.persistence.users.User u where u.firstname = '" + firstname + "'";
+        try {
+            return  HibernateUtil.currentSession().createQuery(sql).list();
+        } finally {
+            HibernateUtil.closeSession();
+        }        
     }
-       public User findByUserName(String username)
-       {
-            String sql = "from User u where u.username = '" + username + "'";
-        Iterator i = net.abbht.lamo.utils.HibernateUtil.currentSession().createQuery(sql).iterate();
-        return (User)i.next();
-       }
+    public List findByTitle(String title) {
+        String sql = "from net.abbht.lamo.persistence.users.User u where u.title = '" + title + "'";
+        try {
+            return  HibernateUtil.currentSession().createQuery(sql).list();
+        } finally {
+            HibernateUtil.closeSession();
+        }        
+    }
+    
+   public List findByEmail(String email) {
+        String sql = "from net.abbht.lamo.persistence.users.User u where u.email = '" + email + "'";
+        try {
+            return  HibernateUtil.currentSession().createQuery(sql).list();
+        } finally {
+            HibernateUtil.closeSession();
+        }        
+    }    
+   
+    public List findByTotalDueDays(String days) {
+        String sql = "from net.abbht.lamo.persistence.users.User u where u.totalduedays = '" + days + "'";
+        try {
+            return  HibernateUtil.currentSession().createQuery(sql).list();
+        } finally {
+            HibernateUtil.closeSession();
+        }        
+    }  
+   
+    public User findByUserName(String username) {
+        String sql = "from net.abbht.lamo.persistence.users.User u where u.username = '" + username + "'";
+        User u = null;
+        try {
+            u = (User) HibernateUtil.currentSession().createQuery(sql).uniqueResult();
+        } finally {
+            HibernateUtil.closeSession();
+        }
+        return u;
+    }
+    
 }
