4df24b70e878cf9f590f437464d90964e40f4d37
mdshannan <mdshannan@afb23650-9424-0410-ac3c-bbcb0118fe35>
2006-12-14 23:23:49 +0000

    created methods for saving and querying phone_types

--- Lib3/src/net/abbht/lamo/facades/users/PhoneFacade.java
+++ Lib3/src/net/abbht/lamo/facades/users/PhoneFacade.java
@@ -1,38 +1,77 @@
 package net.abbht.lamo.facades.users;
 
 import java.util.Iterator;
+import java.util.List;
 import net.abbht.lamo.persistence.users.*;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import net.abbht.lamo.utils.*;
 
 /**
  *  @netbeans.hibernate.facade beanClass=net.abbht.lamo.persistence.users.Phone
  */
-public class PhoneFacade { 
+public class PhoneFacade {
     public void savePhone(Phone phone) {
-        Session session = net.abbht.lamo.utils.HibernateUtil.currentSession();
-        Transaction tx = session.beginTransaction();
-        session.save(phone);
-        tx.commit();
-        net.abbht.lamo.utils.HibernateUtil.closeSession();
-    }    
+        Session session = HibernateUtil.currentSession();
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            session.save(phone);
+        } finally {
+            tx.commit();
+            HibernateUtil.closeSession();
+        }
+    }
     
     public void updatePhone(Phone phone) {
         Session session = net.abbht.lamo.utils.HibernateUtil.currentSession();
-        Transaction tx = session.beginTransaction();
-        session.update(phone);
-        tx.commit();
-        net.abbht.lamo.utils.HibernateUtil.closeSession();
-    }       
-    public Phone findByPrimaryKey(Object key)
-    {
-        return this.findByPrimaryKey(key);
+        Transaction tx = null;
+        try {
+            session.beginTransaction();
+            session.update(phone);
+        } finally {
+            tx.commit();
+            HibernateUtil.closeSession();
+        }
     }
     
-    public Phone findByPhoneNo(String ph)
-    {
-         String sql = "from Phone u where u.phoneNo =" + ph;
-        Iterator i = net.abbht.lamo.utils.HibernateUtil.currentSession().createQuery(sql).iterate();
-        return (Phone)i.next();
+    public Phone findByPhoneNo(String ph) {
+        String sql = "from net.abbht.lamo.persistence.users.Phone u where u.phoneNo ='" + ph + "'";
+        try {
+        return (Phone) HibernateUtil.currentSession().createQuery(sql).uniqueResult();
+        } finally {
+            HibernateUtil.closeSession();
+        }        
     }
+    
+    public List <Phone> findByUsername(String username) {
+        String sql = "from net.abbht.lamo.persistence.users.Phone u where u.username ='" + username + "'";
+        try {
+            return  HibernateUtil.currentSession().createQuery(sql).list();
+        } finally {
+            HibernateUtil.closeSession();
+        }        
+    }
+    
+     public void savePhoneType(PhoneTypes phoneType) {
+        Session session = HibernateUtil.currentSession();
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            session.save(phoneType);
+        } finally {
+            tx.commit();
+            HibernateUtil.closeSession();
+        }
+    }
+        
+    public List <PhoneTypes> findAllPhoneType () {
+        String sql = "from net.abbht.lamo.persistence.users.PhoneTypes";
+        try {
+            return HibernateUtil.currentSession().createQuery(sql).list();
+        } finally {
+            HibernateUtil.closeSession();
+        }        
+    }
+    
 }
