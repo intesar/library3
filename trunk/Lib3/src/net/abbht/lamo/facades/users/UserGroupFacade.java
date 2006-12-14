c4c9ba41112c1c2847942af7394fee4ebefe0ce9
mdshannan <mdshannan@afb23650-9424-0410-ac3c-bbcb0118fe35>
2006-12-14 23:05:07 +0000

    refactored

--- trunk/Lib3/src/net/abbht/lamo/facades/users/UserGroupFacade.java
+++ trunk/Lib3/src/net/abbht/lamo/facades/users/UserGroupFacade.java
@@ -1,39 +1,54 @@
 package net.abbht.lamo.facades.users;
 
 import java.util.Iterator;
+import java.util.List;
 import net.abbht.lamo.persistence.users.*;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-
+import net.abbht.lamo.utils.*;
 /**
  *  @netbeans.hibernate.facade beanClass=net.abbht.lamo.persistence.users.UserGroup
  */
-public class UserGroupFacade { 
+public class UserGroupFacade {
     public void saveUserGroup(UserGroup userGroup) {
-        Session session = net.abbht.lamo.utils.HibernateUtil.currentSession();
-        Transaction tx = session.beginTransaction();
-        session.save(userGroup);
-        tx.commit();
-        net.abbht.lamo.utils.HibernateUtil.closeSession();
+        Session session = HibernateUtil.currentSession();
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            session.save(userGroup);
+        }  finally {
+            tx.commit();
+            HibernateUtil.closeSession();
+        }
     }
     
     public void updateUserGroup(UserGroup userGroup) {
         Session session = net.abbht.lamo.utils.HibernateUtil.currentSession();
-        Transaction tx = session.beginTransaction();
-        session.update(userGroup);
-        tx.commit();
-        net.abbht.lamo.utils.HibernateUtil.closeSession();
-    }    
-    public UserGroup findByPrimaryKey(Object key)
-    {
-        return this.findByPrimaryKey(key);
-        
+        Transaction tx = null;
+        try {
+            tx = session.beginTransaction();
+            session.update(userGroup);
+        }  finally {
+            tx.commit();
+            HibernateUtil.closeSession();
+        }
     }
     
-    public UserGroup findByGroupName(String gname)
-    {
-         String sql = "from UserGroup u where u.groupname =" + gname;
-        Iterator i = net.abbht.lamo.utils.HibernateUtil.currentSession().createQuery(sql).iterate();
-        return (UserGroup)i.next();
+    
+    public UserGroup findByGroupName(String gname) {
+        String sql = "from net.abbht.lamo.persistence.users.UserGroup u where u.groupname = '" + gname + "'";
+        try {
+            return ( UserGroup ) HibernateUtil.currentSession().createQuery(sql).uniqueResult();
+        }  finally {
+            HibernateUtil.closeSession();
+        }
     }
+    public List findAll () {
+        String sql = "from net.abbht.lamo.persistence.users.UserGroup";
+        try {
+            return HibernateUtil.currentSession().createQuery(sql).list();
+        }  finally {
+            HibernateUtil.closeSession();
+        }
+    }    
 }
