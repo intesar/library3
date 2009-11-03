ec42b7d1876029128318a2c3be4c0e3e1f8b44ae
mdshannan <mdshannan@afb23650-9424-0410-ac3c-bbcb0118fe35>
2009-11-03 00:09:22 +0000

    updates

---
+++ CCM/web/js/emailtimings.js
@@ -0,0 +1,65 @@
+var peopleCache = { };
+var viewed = null;
+jq(document).ready(function() {
+    fillTable();
+
+
+    function fillTable() {
+        AjaxAdminService.getAllEmailTimePreference(function(people) {
+            // Delete all the rows except for the "pattern" row
+            dwr.util.removeAllRows("peoplebody", {
+                filter:function(tr) {
+                    return (tr.id != "pattern");
+                }
+                });
+            // Create a new set cloned from the pattern row
+            var person, id;
+            people.sort(function(p1, p2) {
+                return p1.reportTime - p2.reportTime;
+            });
+            for (var i = 0; i < people.length; i++) {
+                person = people[i];
+                id = person.id;
+                dwr.util.cloneNode("pattern", {
+                    idSuffix:id
+                });
+                dwr.util.setValue("reporttime" + id, person.reportTime);
+                $("pattern" + id).style.display = "";
+                peopleCache[id] = person;
+            }
+        });
+    }
+
+    function deleteClicked(eleid) {
+
+        var emailTime = peopleCache[eleid.substring(6)];
+        //alert ( emailTime.id );
+        AjaxAdminService.deleteEmailTimePreference(emailTime, function(data) {
+            if ( data != "Deleted Successfully") {
+                writeMessage("successReply", "Operation Successful!");
+                fillTable();
+            }
+            else {
+                writeMessage ( "failureReply", "Operation Failed, Please try again!" );
+
+            }
+        });
+    }
+    function writePerson() {
+        var person = {
+            id:null,
+            reportTime:null,
+            organization:null
+        };
+        dwr.util.getValues(person);
+        AjaxAdminService.saveEmailTimePreference(person, function(data) {
+            if ( data == "Added Successfully!") {
+                writeMessage("successReply", "Operation Successful!");
+                fillTable();
+            }
+            else {
+                writeMessage ( "failureReply", "Operation Failed, Please try again!" );
+            }
+        });
+    }
+});
---
+++ CCM/web/js/my_systemlease.js
@@ -0,0 +1,62 @@
+function execute() {
+    dwr.util.useLoadingMessage("Please wait, Loading");
+    //alert ( document.getElementById("startDate").value);
+    var startDate = document.getElementById("DPC_startDate_YYYY-MM-DD").value;
+    var endDate = document.getElementById("DPC_endDate_YYYY-MM-DD").value;
+    if ( startDate != null && startDate.length == 10 && endDate != null && endDate.length == 10 ) {
+        AjaxCustomerService.getMySystemLease(startDate,endDate, reply1 );
+    } else {
+        alert ( " invalid dates ");
+    }
+}
+
+
+var peopleCache = { };
+var viewed = null;
+
+var reply1 = function(people) {
+    // Delete all the rows except for the "pattern" row
+    dwr.util.removeAllRows("peoplebody", {
+        filter:function(tr) {
+            return (tr.id != "pattern");
+        }
+        });
+    // Create a new set cloned from the pattern row
+    var person, id;
+    if ( people.length == 0) {
+        writeMessage ( "failureReply", "No recoreds found!");
+    } else  {
+        //people.sort(function(p1, p2) { return p1.macAddress.localeCompare(p2.macAddress); });
+        for (var i = 0; i < people.length; i++) {
+            person = people[i];
+            id = person.id;
+            dwr.util.cloneNode("pattern", {
+                idSuffix:id
+            });
+            dwr.util.setValue("leaseHolderName" + id, person.leaseHolderName);
+            dwr.util.setValue("system" + id, person.service);
+            dwr.util.setValue("startTimeString" + id, person.startTimeString);
+            dwr.util.setValue("endTimeString" + id, person.endTimeString);
+            dwr.util.setValue("totalMinutesUsed" + id, person.totalMinutesUsed);
+            dwr.util.setValue("payableAmount" + id, person.payableAmount);
+            dwr.util.setValue("amountPaid" + id, person.amountPaid);
+            var maxLength = 0;
+            var issueAgentEmail = person.issueAgent;
+            if ( issueAgentEmail != null && issueAgentEmail.length > 0 ) {
+                maxLength = issueAgentEmail.toString().indexOf("@");
+                maxLength = maxLength <= 14 ? maxLength : 14;
+                dwr.util.setValue("issueAgent" + id, issueAgentEmail.toString().substring(0,maxLength));
+            }
+            var issueReturnedEmail = person.returnAgent;
+            if ( issueReturnedEmail != null && issueReturnedEmail.length > 0 ) {
+                maxLength = issueReturnedEmail.toString().indexOf("@");
+                maxLength = maxLength <= 14 ? maxLength : 14;
+                dwr.util.setValue("returnAgent" + id, issueReturnedEmail.toString().substring(0,maxLength));
+            }
+            $("pattern" + id).style.display = "";
+            peopleCache[id] = person;
+        }
+    }
+
+}
+        
