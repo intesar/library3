0ca9ccdcc1cc2660d2e99c02ddada2b8b10a9d8b
mdshannan <mdshannan@afb23650-9424-0410-ac3c-bbcb0118fe35>
2010-07-09 20:45:36 +0000

    moved ajax package to root package

---
+++ trunk/CCM/src/java/com/bia/ccm/ajax/AdminAjaxService.java
@@ -0,0 +1,320 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bia.ccm.ajax;
+
+import com.abbhsoft.sqlInjectionFilter.SQLInjectionFilterManager;
+import com.bia.ccm.entity.EmailPreference;
+import com.bia.ccm.entity.EmailTimePreference;
+import com.bia.ccm.entity.Organization;
+import com.bia.ccm.entity.Services;
+import com.bia.ccm.entity.SystemLease;
+import com.bia.ccm.entity.Systems;
+import com.bia.ccm.entity.Users;
+import com.bia.ccm.entity.UsersLight;
+import com.bia.ccm.services.AdminService;
+import com.bia.ccm.util.AcegiUtil;
+import com.bia.ccm.util.ServiceFactory;
+
+import com.bia.converter.CaseConverter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ *
+ * @author intesar
+ */
+public class AdminAjaxService {
+
+    public String deleteEmail(int id) {
+        String msg = "Deleted Email Successful!";
+        try {
+            this.adminService.deleteEmail(id);
+            return msg;
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            return e.getMessage();
+        }
+    }
+
+    public String updateRentalPrice(int mims, double rate, Integer lmins, Double lrate) {
+        String msg = "Price Updated Successful!";
+        if (logger.isDebugEnabled()) {
+            logger.debug("updateRentalPrice ------ ");
+        }
+        String username = AcegiUtil.getUsername();
+        if (logger.isDebugEnabled()) {
+            logger.debug("updateRentalPrice ------ " + username);
+        }
+        if (mims < 0 || rate < 0.0 || (lmins != null && lmins < 0) || (lrate != null && lrate < 0.0)) {
+            return "We are keeping an eye on you! ";
+        }
+        if (logger.isDebugEnabled()) {
+            logger.debug("updateRentalPrice ------ after validation");
+        }
+        try {
+            this.adminService.updateRentalPrice(mims, rate, lmins, lrate, username);
+            return msg;
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            return e.getMessage();
+        }
+    }
+
+    public List<Systems> getAllSystems() {
+        String username = AcegiUtil.getUsername();
+        return this.adminService.getAllSystems(username);
+    }
+
+    public String saveSystems(Systems systems) {
+        String msg = "Operation succesful!";
+        try {
+            String username = AcegiUtil.getUsername();
+            this.adminService.saveSystem(systems, username);
+        } catch (Exception e) {
+            logger.error(e);
+            return e.getMessage();
+        }
+        return msg;
+    }
+
+    public List<Users> getAllUsers() {
+        String username = AcegiUtil.getUsername();
+        List<Users> users = null;
+        users = this.adminService.getAllUsers(username);
+        for (Users u : users) {
+            u.setPic(null);
+        }
+        return users;
+    }
+
+    public String saveUsers(Users users) {
+        logger.debug("inside ------------------ save users ");
+        String msg = "Operation succesful!";
+        users.setEmail(SQLInjectionFilterManager.getInstance().filter(users.getEmail()));
+        users.setUsername(SQLInjectionFilterManager.getInstance().filter(users.getUsername()));
+        logger.debug("inside ------------------ save users1 ");
+        try {
+            String username = AcegiUtil.getUsername();
+            this.adminService.saveUser(users, username);
+            logger.debug("inside ------------------ save users 2 ");
+        } catch (Exception e) {
+            logger.error(e);
+            logger.debug("inside ------------------ save users 3 " + e.getMessage());
+            return e.getMessage();
+        }
+        return msg;
+    }
+
+    public List<Users> saveAndGetUsers(Users users) {
+        saveUsers(users);
+        return getAllUsers();
+    }
+
+    public List<EmailPreference> getAllEmailPreference() {
+        String username = AcegiUtil.getUsername();
+        return this.adminService.getAllEmailPreference(username);
+    }
+
+    public String saveEmailPreference(EmailPreference emailPreference) {
+        String msg = "Operation succesful!";
+        try {
+            String username = AcegiUtil.getUsername();
+            emailPreference.setEmailOrPhone(SQLInjectionFilterManager.getInstance().filter(emailPreference.getEmailOrPhone()));
+            emailPreference.setEmailOrPhone(emailPreference.getEmailOrPhone().toLowerCase());
+            if (emailPreference.getUsername() != null) {
+                emailPreference.setUsername(emailPreference.getUsername().toLowerCase());
+            }
+            this.adminService.saveEmailPreference(emailPreference, username);
+        } catch (Exception e) {
+            logger.error(e);
+            return e.getMessage();
+        }
+        return msg;
+    }
+
+    public List<EmailTimePreference> getAllEmailTimePreference() {
+        String username = AcegiUtil.getUsername();
+        return this.adminService.getAllEmailTimePreference(username);
+    }
+
+    public String saveEmailTimePreference(EmailTimePreference emailTimePreference) {
+        String username = AcegiUtil.getUsername();
+        try {
+            this.adminService.saveEmailTimePreference(emailTimePreference, username);
+            return "Added Successfully!";
+        } catch (Exception e) {
+            logger.error(e);
+            return e.getMessage();
+        }
+    }
+
+    public String deleteEmailTimePreference(EmailTimePreference emailTimePreference) {
+        try {
+            this.adminService.deleteEmailTimePreference(emailTimePreference);
+            return "Deleted Successfully";
+        } catch (Exception e) {
+            logger.error(e);
+            return e.getMessage();
+        }
+    }
+
+    public List<SystemLease> getAllSystemLease() {
+        String username = AcegiUtil.getUsername();
+
+        return this.adminService.getAllSystemLease(username);
+
+    }
+
+    public Organization getOrganization() {
+        String username = AcegiUtil.getUsername();
+        return this.adminService.getOrganization(username);
+    }
+
+    public String saveOrganization(Organization organization) {
+        String msg = "Company profile saved successfully!";
+        try {
+            String username = AcegiUtil.getUsername();
+            this.adminService.saveOrganization(organization, username);
+        } catch (Exception e) {
+            logger.error(e);
+//            return e.getMessage();
+            msg = "Error please try again!";
+        }
+        return msg;
+    }
+
+    public List<SystemLease> getSystemLease(String startDateString, String endDateString) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date startDate = null;
+        Date endDate = null;
+        String username = AcegiUtil.getUsername();
+        UsersLight u = this.adminService.getUserByUsername(username);
+
+        try {
+            startDate = sdf.parse(startDateString);
+            endDate = sdf.parse(endDateString);
+            return this.adminService.getSystemLease(startDate, endDate, u.getOrganization());
+        } catch (ParseException ex) {
+            Logger.getLogger(AdminAjaxService.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+
+    }
+
+    public List getReport(String startDateString, String endDateString) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            Date startDate = sdf.parse(startDateString);
+            Date endDate = sdf.parse(endDateString);
+            logger.debug(startDate + " " + endDate);
+            String username = AcegiUtil.getUsername();
+            UsersLight u = this.adminService.getUserByUsername(username);
+            return this.adminService.getReport(startDate, endDate, u.getOrganization());
+        } catch (ParseException ex) {
+            Logger.getLogger(AdminAjaxService.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+
+    }
+
+    public String saveService(Services service) {
+        if (service.getId() == -5) {
+            if (logger.isDebugEnabled()) {
+                logger.debug("computer rate save ------ " + service.getUnits() + "  " + service.getUnitPrice());
+                logger.debug("computer rate ------ " + service.getSaleTwoUnits() + " " + service.getSaleTwoPrice());
+            }
+            try {
+                this.updateRentalPrice(service.getUnits(), service.getUnitPrice(),
+                        service.getSaleTwoUnits(), service.getSaleTwoPrice());
+                return " Updated rates successfully ! ";
+            } catch (NullPointerException ex) {
+                ex.printStackTrace();
+            }
+        }
+        try {
+            service.setOrganization(this.getOrganization().getName());
+            adminService.saveService(service);
+            return " Service saved successful! ";
+        } catch (Exception e) {
+            logger.error(e);
+            return "Error please try again!";
+        }
+    }
+
+    public String deleteService(Integer id) {
+        try {
+//            System.out.println ( "delete");
+            adminService.deleteService(id, getOrganization().getName());
+//            return "Service deleted successful!";
+        } catch (Exception e) {
+            logger.error(e);
+        }
+        return "Service deleted successful!";
+    }
+
+    /**
+     * add computer as service to this list
+     * @return
+     */
+    public List<Services> getAllServices() {
+        String org = this.getOrganization().getName();
+        List<Services> list = this.adminService.getAllServices(org);
+        Systems system = this.adminService.getSystem(org);
+        Services s = new Services();
+        s.setId(-5);
+        s.setName("Computer");
+        s.setUnits(system.getMinimumMinutes());
+        s.setUnitPrice(system.getMinuteRate());
+        s.setSaleTwoUnits(system.getLowerMinimumMinutes());
+        s.setSaleTwoPrice(system.getLowerMinuteRate());
+        list.add(s);
+        return list;
+    }
+
+    public void sendReports() {
+        this.adminService.sendReports();
+    }
+
+    public void setCaseConverter(CaseConverter caseConverter) {
+        this.caseConverter = caseConverter;
+    }
+    private CaseConverter caseConverter;
+    protected final Log logger = LogFactory.getLog(getClass());
+    private AdminService adminService = (AdminService) ServiceFactory.getService("adminServiceImpl");
+
+    public static void main(String[] args) {
+        AdminAjaxService aas = new AdminAjaxService();
+//        EmailPreference ep = new EmailPreference(null, "test", "mohdshannan@yahoo.com", "bia");
+//        aas.saveEmailPreference(ep);
+//        short s = (short) 500;
+//        EmailTimePreference etp = new EmailTimePreference(null, s, "bia");
+//        aas.saveEmailTimePreference(etp);
+//        Systems s1 = new Systems(null, 2, "bia", true, null, 20.8, true);
+//        aas.saveSystems(s1);
+//        Users u = new Users(null, "shannan", "shannan", true, "admin", "bia", "shannan");
+//        aas.saveUsers(u);
+//        
+//        System.out.println(aas.getAllEmailPreference());
+//        System.out.println(aas.getAllEmailTimePreference());
+//        System.out.println(aas.getAllSystemLease());
+//        System.out.println(aas.getAllSystems());
+//        System.out.println(aas.getAllUsers());
+//        System.out.println(aas.getOrganization());
+        Date dt1 = new Date(107, 7, 7);
+        Date dt2 = new Date(110, 11, 11);
+
+        //System.out.println(aas.getSystemLease("2007-07-07", "2008-10-10").size());
+        //System.out.println ();//getReport("2008-08-04", "2008-08-04").toString());
+        // System.out.println(aas.getReport("2007-07-07", "2008-10-10"));
+        aas.sendReports();
+    }
+}
