7deb070ffe6e6539fc7aa136ee6727e5e6aae2f1
mdshannan <mdshannan@afb23650-9424-0410-ac3c-bbcb0118fe35>
2010-07-30 03:43:15 +0000

    Copied remotely

---
+++ CCPortlets/CyberCafePortlets/src/java/com/bia/ccm/ajax/AjaxAdminService.java
@@ -0,0 +1,438 @@
+/*
+ * 
+ */
+package com.bia.ccm.ajax;
+
+import com.bia.ccm.entity.EmailPreference;
+import com.bia.ccm.entity.EmailTimePreference;
+import com.bia.ccm.entity.Organization;
+import com.bia.ccm.entity.Services;
+import com.bia.ccm.entity.SystemLease;
+import com.bia.ccm.entity.Systems;
+import com.bia.ccm.entity.Users;
+import com.bia.ccm.exceptions.InvalidInputException;
+import com.bia.ccm.exceptions.NoRoleException;
+import com.bia.ccm.services.AdminService;
+import com.bia.ccm.util.AcegiUtil;
+import com.bia.ccm.util.ServiceFactory;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * @see  src/context/ApplicationContext-AjaxService.xml
+ *
+ *  This class is mapped to AjaxAdminService bean in the above file
+ *
+ * @author intesar
+ */
+public class AjaxAdminService {
+
+    /**
+     *   Response codes
+     *   1 == Operation Successfully executed
+     *  -1 == Operation execution failed, no reason specified, ask user to try again
+     *  -2 == Operation not executed, because of invalid input
+     *  -3 == Operation failed (User doesn't have appropriate roles on data), if user believes he has roles ask him to refresh page or relogin
+     *
+     * only Owner can delete Notification Email
+     * @param id
+     * @return
+     */
+    public int deleteEmail(int id) {
+        try {
+            this.adminService.deleteEmail(id, AcegiUtil.getUsername());
+            return 1;
+        } catch (InvalidInputException ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -2;
+        } catch (NoRoleException ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -3;
+        } catch (Exception ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -1;
+        }
+    }
+
+    /**
+     *
+     * @param mims
+     * @param rate
+     * @param lmins
+     * @param lrate
+     * @param request
+     * @return
+     */
+    public int updateRentalPrice(int mims, double rate, Integer lmins, Double lrate, HttpServletRequest request) {
+        try {
+            String username = AcegiUtil.getUsername();
+            this.adminService.updateRentalPrice(mims, rate, lmins, lrate, username, request.getRemoteAddr());
+            return 1;
+        } catch (InvalidInputException ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -2;
+        } catch (NoRoleException ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -3;
+        } catch (Exception ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -1;
+        }
+    }
+
+    /**
+     *
+     * @return
+     */
+    public List<Systems> getAllSystems() {
+        try {
+            String username = AcegiUtil.getUsername();
+            return this.adminService.getAllSystems(username);
+        } catch (Exception ex) {
+            logger.warn(ex.getMessage(), ex);
+        }
+        return null;
+    }
+
+    /**
+     *
+     * @param systems
+     * @return
+     */
+    public int saveSystems(Systems systems) {
+        try {
+            String username = AcegiUtil.getUsername();
+            this.adminService.saveSystem(systems, username);
+            return 1;
+        } catch (InvalidInputException ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -2;
+        } catch (NoRoleException ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -3;
+        } catch (Exception ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -1;
+        }
+    }
+
+    /**
+     * returns all CC employees
+     * @return
+     */
+    public List<Users> getAllUsers() {
+        try {
+            String username = AcegiUtil.getUsername();
+            List<Users> users = this.adminService.getAllUsers(username);
+            return users;
+        } catch (Exception ex) {
+            logger.warn(ex.getMessage(), ex);
+            return null;
+        }
+    }
+
+    /**
+     *
+     * @param users
+     * @return
+     */
+    public int saveUsers(Users users) {
+        try {
+            String username = AcegiUtil.getUsername();
+            this.adminService.saveUser(users, username);
+            return 1;
+        } catch (InvalidInputException ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -2;
+        } catch (NoRoleException ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -3;
+        } catch (Exception ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -1;
+        }
+    }
+
+    /**
+     *
+     * @param users
+     * @return
+     */
+    public List<Users> saveAndGetUsers(Users users) {
+        saveUsers(users);
+        return getAllUsers();
+    }
+
+    /**
+     *
+     * @return
+     */
+    public List<EmailPreference> getAllEmailPreference() {
+        try {
+            String username = AcegiUtil.getUsername();
+            return this.adminService.getAllEmailPreference(username);
+        } catch (Exception ex) {
+            logger.warn(ex.getMessage(), ex);
+            return null;
+        }
+    }
+
+    /**
+     *
+     * @param emailPreference
+     * @return
+     */
+    public int saveEmailPreference(EmailPreference emailPreference) {
+        try {
+            String username = AcegiUtil.getUsername();
+            this.adminService.saveEmailPreference(emailPreference, username);
+            return 1;
+        } catch (InvalidInputException ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -2;
+        } catch (NoRoleException ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -3;
+        } catch (Exception ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -1;
+        }
+    }
+
+    /**
+     *
+     * @return
+     */
+    public List<EmailTimePreference> getAllEmailTimePreference() {
+        try {
+            String username = AcegiUtil.getUsername();
+            return this.adminService.getAllEmailTimePreference(username);
+        } catch (Exception ex) {
+            logger.warn(ex.getMessage(), ex);
+            return null;
+        }
+    }
+
+    /**
+     *
+     * @param emailTimePreference
+     * @return
+     */
+    public int saveEmailTimePreference(EmailTimePreference emailTimePreference) {
+        try {
+            String username = AcegiUtil.getUsername();
+            this.adminService.saveEmailTimePreference(emailTimePreference, username);
+            return 1;
+        } catch (InvalidInputException ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -2;
+        } catch (NoRoleException ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -3;
+        } catch (Exception ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -1;
+        }
+    }
+
+    /**
+     *
+     * @param emailTimePreference
+     * @return
+     */
+    public int deleteEmailTimePreference(EmailTimePreference emailTimePreference) {
+        try {
+            this.adminService.deleteEmailTimePreference(emailTimePreference, AcegiUtil.getUsername());
+            return 1;
+        } catch (InvalidInputException ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -2;
+        } catch (NoRoleException ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -3;
+        } catch (Exception ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -1;
+        }
+    }
+
+    /**
+     *  not in use
+     * @return
+     */
+    public List<SystemLease> getAllSystemLease() {
+        String username = AcegiUtil.getUsername();
+
+        return this.adminService.getAllSystemLease(username);
+
+    }
+
+    /**
+     *
+     * @return
+     */
+    public Organization getOrganization() {
+        try {
+            String username = AcegiUtil.getUsername();
+            return this.adminService.getOrganization(username);
+        } catch (Exception ex) {
+            logger.warn(ex.getMessage(), ex);
+            return null;
+        }
+    }
+
+    /**
+     * 
+     * @param organization
+     * @return
+     */
+    public int saveOrganization(Organization organization) {
+        try {
+            String username = AcegiUtil.getUsername();
+            this.adminService.saveOrganization(organization, username);
+            return 1;
+        } catch (InvalidInputException ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -2;
+        } catch (NoRoleException ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -3;
+        } catch (Exception ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -1;
+        }
+    }
+
+    /**
+     *
+     * @param startDateString
+     * @param endDateString
+     * @return
+     */
+    public List<SystemLease> getSystemLease(String startDateString, String endDateString) {
+        try {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            Date startDate = sdf.parse(startDateString);
+            Date endDate = sdf.parse(endDateString);
+            String username = AcegiUtil.getUsername();
+            return this.adminService.getSystemLease(startDate, endDate, username);
+        } catch (ParseException ex) {
+            logger.warn(ex.getMessage(), ex);
+        } catch (Exception ex) {
+            logger.warn(ex.getMessage(), ex);
+        }
+        return null;
+    }
+
+    /**
+     *
+     * @param startDateString
+     * @param endDateString
+     * @return
+     */
+    public List getReport(String startDateString, String endDateString) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            Date startDate = sdf.parse(startDateString);
+            Date endDate = sdf.parse(endDateString);
+            String username = AcegiUtil.getUsername();
+            return this.adminService.getReport(startDate, endDate, username);
+        } catch (ParseException ex) {
+            logger.warn(ex.getMessage(), ex);
+            } catch (Exception ex) {
+            logger.warn(ex.getMessage(), ex);
+        }
+        return null;
+    }
+
+    /**
+     * 
+     * @param service
+     * @param request
+     * @return
+     */
+    public int saveService(Services service, HttpServletRequest request) {
+        try {
+            this.adminService.saveService(service, AcegiUtil.getUsername(), request.getRemoteAddr());
+            return 1;
+        } catch (InvalidInputException ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -2;
+        } catch (NoRoleException ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -3;
+        } catch (Exception ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -1;
+        }
+    }
+
+    /**
+     * 
+     * @param id
+     * @return
+     */
+    public int deleteService(Integer id) {
+        try {
+            adminService.deleteService(id, AcegiUtil.getUsername());
+            return 1;
+        } catch (InvalidInputException ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -2;
+        } catch (NoRoleException ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -3;
+        } catch (Exception ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -1;
+        }
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public List<Services> getAllServices() {
+        try {
+            List<Services> list = this.adminService.getAllServices(AcegiUtil.getUsername());
+            return list;
+        } catch (Exception ex) {
+            logger.warn(ex.getMessage(), ex);
+            return null;
+        }
+    }
+
+    /**
+     * not in use
+     * @return
+     */
+    public int sendReports() {
+        try {
+            this.adminService.sendReports();
+            return 1;
+        } catch (InvalidInputException ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -2;
+        } catch (NoRoleException ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -3;
+        } catch (Exception ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -1;
+        }
+    }
+
+    public void setAdminService(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+    protected static final Log logger = LogFactory.getLog(AjaxAdminService.class);
+    protected AdminService adminService = (AdminService) ServiceFactory.getService("adminServiceImpl");
+}
---
+++ CCPortlets/CyberCafePortlets/src/java/com/bia/ccm/ajax/AjaxWorkService.java
@@ -0,0 +1,429 @@
+/*
+ * 
+ */
+package com.bia.ccm.ajax;
+
+import com.abbhsoft.sqlInjectionFilter.SQLInjectionFilterManager;
+import com.bia.ccm.entity.MembershipDiscounts;
+import com.bia.ccm.entity.MembershipTypes;
+import com.bia.ccm.entity.Memberships;
+import com.bia.ccm.entity.Organization;
+import com.bia.ccm.entity.Services;
+import com.bia.ccm.entity.SystemLease;
+import com.bia.ccm.entity.Systems;
+import com.bia.ccm.entity.UsageDetail;
+import com.bia.ccm.entity.Users;
+import com.bia.ccm.entity.UsersLight;
+import com.bia.ccm.exceptions.InvalidInputException;
+import com.bia.ccm.exceptions.NoRoleException;
+import com.bia.ccm.services.AdminService;
+import com.bia.ccm.services.MembershipService;
+import com.bia.ccm.services.WorkService;
+import com.bia.ccm.util.AcegiUtil;
+import com.bia.ccm.util.ServiceFactory;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ *
+ * @author intesar
+ */
+public class AjaxWorkService {
+
+    /**
+     * 
+     * @return
+     */
+    public List<Services> getAllServices() {
+        try {
+            return this.workService.getAllServices(AcegiUtil.getUsername());
+        } catch (Exception ex) {
+            logger.warn(ex.getMessage(), ex);
+        }
+        return null;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public List<Systems> getActiveSystems() {
+        try {
+            String username = AcegiUtil.getUsername();
+            return workService.getActiveSystems(username);
+        } catch (Exception ex) {
+            logger.warn(ex.getMessage(), ex);
+        }
+        return null;
+    }
+
+    /**
+     * not in use
+     * @param systemNo
+     * @return
+     */
+    public Systems getSystemByNameAndOrganization(int systemNo) {
+        String username = AcegiUtil.getUsername();
+        return this.workService.getSystemByNameAndOrganization(systemNo, username);
+    }
+
+    /**
+     *
+     * @param systemId
+     * @param leaseHolder
+     * @return
+     */
+    public int leaseSystem(int systemId, String leaseHolder) {
+        try {
+            this.workService.leaseSystem(systemId, leaseHolder, AcegiUtil.getUsername());
+            return 1;
+        } catch (InvalidInputException ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -2;
+        } catch (NoRoleException ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -3;
+        } catch (Exception ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -1;
+        }
+
+    }
+
+    /**
+     * 
+     * @param service
+     * @param units
+     * @param user
+     * @param payableAmount
+     * @param comments
+     * @param paidAmount
+     * @return
+     */
+    public int addService(String service, Long units, String user, Double payableAmount,
+            String comments, Double paidAmount) {
+
+        try {
+            String agent = AcegiUtil.getUsername();
+            this.workService.addService(service, units, user, payableAmount, comments, paidAmount, agent);
+            return 1;
+        } catch (InvalidInputException ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -2;
+        } catch (NoRoleException ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -3;
+        } catch (Exception ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -1;
+        }
+
+    }
+
+    /**
+     *
+     * @param id
+     * @return
+     */
+    public UsageDetail getPayableAmount(int id) {
+        try {
+            return this.workService.getPayableAmount(id);
+        } catch (Exception ex) {
+            logger.warn(ex.getMessage(), ex);
+            return null;
+        }
+    }
+
+    /**
+     *
+     * @param systemId
+     * @param paidAmount
+     * @return
+     */
+    public int unleaseSystem(int systemId, double paidAmount) {
+        try {
+            this.workService.unleaseSystem(systemId, paidAmount, AcegiUtil.getUsername());
+            return 1;
+        } catch (InvalidInputException ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -2;
+        } catch (NoRoleException ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -3;
+        } catch (Exception ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -1;
+        }
+    }
+
+    /**
+     *
+     * @param c
+     * @return
+     */
+    public int createCustomer(Users c) {
+        try {
+            c.setCreateDate(new Date());
+            Users u = this.workService.getCustomer(AcegiUtil.getUsername());
+            c.setIsVerified(true);
+            c.setVerifiedBy(u.getUsername());
+            //c.setCreateUser(AcegiUtil.getUsername());
+            this.workService.createCutomer(c, u);
+            return 1;
+        } catch (InvalidInputException ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -2;
+        } catch (NoRoleException ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -3;
+        } catch (Exception ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -1;
+        }
+    }
+
+    /**
+     *
+     * @param key
+     * @return
+     */
+    public Users getUserWithPic(String key) {
+        try {
+            Users c = null;
+            key = SQLInjectionFilterManager.getInstance().filter(key);
+            try {
+                if (key != null) {
+                    key = key.toLowerCase();
+                }
+                c = this.workService.getCustomerPic(key);
+                //c.setPassword("Encrypted");
+            } catch (Exception ex) {
+                logger.warn(ex.getMessage(), ex);
+            }
+            if (c == null) {
+                c = new Users();
+            }
+            return c;
+        } catch (Exception ex) {
+            logger.warn(ex.getMessage(), ex);
+            return null;
+        }
+    }
+
+    /**
+     *
+     * @param key
+     * @return
+     */
+    public Users getCustomer(String key) {
+        try {
+            Users c = null;
+            key = SQLInjectionFilterManager.getInstance().filter(key);
+            try {
+                if (key != null) {
+                    key = key.toLowerCase();
+                }
+                c = this.workService.getCustomer(key);
+                //c.setPassword("Encrypted");
+            } catch (Exception ex) {
+                logger.warn(ex.getMessage(), ex);
+            }
+            if (c == null) {
+                c = new Users();
+                c.setComments("No record found for given email!");
+            }
+            return c;
+        } catch (Exception ex) {
+            logger.warn(ex.getMessage(), ex);
+            return null;
+        }
+    }
+
+    /**
+     *
+     * @param id
+     * @return
+     */
+    public List<SystemLease> getSystemLease(int id) {
+        try {
+            return this.workService.getSystemLease(id);
+        } catch (Exception ex) {
+            logger.warn(ex.getMessage(), ex);
+            return null;
+        }
+    }
+
+    /**
+     *
+     * @param systemId
+     * @return
+     */
+    public int chargePayment(int systemId) {
+        try {
+            workService.chargePayment(systemId, AcegiUtil.getUsername());
+            return 1;
+        } catch (InvalidInputException ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -2;
+        } catch (NoRoleException ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -3;
+        } catch (Exception ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -1;
+        }
+    }
+
+    /**
+     *
+     * @return
+     */
+    public Organization getOrganization() {
+        String username = AcegiUtil.getUsername();
+        return this.adminService.getOrganization(username);
+    }
+    // membership for next milestone
+    public List<MembershipTypes> getAllMembershipTypes() {
+        String org = this.getOrganization().getName();
+        List<MembershipTypes> list = this.membershipService.getMembershipTypesByOrganization(org);
+        return list;
+    }
+
+    public int saveMembershipType(MembershipTypes membershipTypes) {
+        try {
+            if (membershipTypes == null || membershipTypes.getName() == null
+                    || membershipTypes.getName().trim().length() <= 0
+                    || membershipTypes.getFee() <= 0.0 || membershipTypes.getDaysValidFor() <= 0) {
+                //return "Invalid Inputs!";
+            }
+            Organization o = this.getOrganization();
+            if (membershipTypes.getId() == null) {
+                membershipTypes.setCreateDate(new Date());
+                membershipTypes.setCreateUser(AcegiUtil.getUsername());
+                membershipTypes.setOrganization(o);
+                membershipTypes.setName(membershipTypes.getName().toLowerCase());
+                this.membershipService.saveMembershipType(membershipTypes);
+
+                membershipTypes = this.membershipService.getMembershipTypesByOrganizationAndName(o.getName(), membershipTypes.getName());
+                List<Services> serviceses = this.getAllServices();
+                if (serviceses == null) {
+                    serviceses = new ArrayList<Services>();
+                }
+                serviceses.add(new Services(0, "computer", 0, ""));
+
+                for (Services s : serviceses) {
+                    MembershipDiscounts md = new MembershipDiscounts(null, s.getName(),
+                            0.0, new Date(), AcegiUtil.getUsername());
+                    md.setMembershipType(membershipTypes);
+                    this.membershipService.saveMembershipDiscounts(md);
+                }
+            } else {
+                this.membershipService.saveMembershipType(membershipTypes);
+            }
+            return 1;
+        } catch (InvalidInputException ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -2;
+        } catch (NoRoleException ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -3;
+        } catch (Exception ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -1;
+        }
+    }
+
+    /**
+     * 
+     * @param membershipType
+     * @return
+     */
+    public List<MembershipDiscounts> getMembershipDiscountses(Integer membershipType) {
+        return this.membershipService.getMembershipDiscountsByMembershipTypesId(membershipType);
+    }
+
+    public void createMembershipDiscount(String service) {
+        String org = this.getOrganization().getName();
+        List<MembershipTypes> list = this.membershipService.getMembershipTypesByOrganization(org);
+        for (MembershipTypes mt : list) {
+            MembershipDiscounts md = new MembershipDiscounts(null, service, 0.0, new Date(), AcegiUtil.getUsername());
+            md.setMembershipType(mt);
+            this.membershipService.saveMembershipDiscounts(md);
+        }
+    }
+
+    public int saveMembershipDiscount(int membershipDiscountId, double discountPercentage) {
+        try {
+            if (discountPercentage < 0.0 || discountPercentage > 100.0) {
+                //return "Discount Percentage cannot be Negative or Greater than 100!";
+            }
+            MembershipDiscounts discounts = this.membershipService.getMembershipDiscountsById(membershipDiscountId);
+            discounts.setDiscountPercentage(discountPercentage);
+            this.membershipService.saveMembershipDiscounts(discounts);
+            return 1;
+        } catch (InvalidInputException ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -2;
+        } catch (NoRoleException ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -3;
+        } catch (Exception ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -1;
+        }
+    }
+
+    public int saveMembership(Memberships memberships) {
+        UsersLight ul = this.adminService.getUserByUsername(memberships.getEmail());
+        if (ul == null || ul.getId() == null) {
+            //return "No user found with this email";
+        }
+        try {
+            this.membershipService.validateUserMembership(memberships.getEmail(), this.getOrganization().getName());
+
+            this.membershipService.saveMembership(memberships, this.getOrganization(), AcegiUtil.getUsername());
+            return 1;
+        } catch (InvalidInputException ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -2;
+        } catch (NoRoleException ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -3;
+        } catch (Exception ex) {
+            logger.warn(ex.getMessage(), ex);
+            return -1;
+        }
+    }
+
+    public List<Memberships> getMembers() {
+        List<Memberships> list = new ArrayList<Memberships>();
+        try {
+            list = this.membershipService.getMembershipsByOrganization(this.getOrganization().getName());
+        } catch (RuntimeException re) {
+        }
+        return list;
+    }
+
+    public void setAdminService(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+    public void setMembershipService(MembershipService membershipService) {
+        this.membershipService = membershipService;
+    }
+
+    public void setWorkService(WorkService workService) {
+        this.workService = workService;
+    }
+    // end of memberships
+
+    
+    protected AdminService adminService = (AdminService) ServiceFactory.getService("adminServiceImpl");
+    protected MembershipService membershipService = (MembershipService) ServiceFactory.getService("membershipServiceImpl");
+    protected WorkService workService = (WorkService) ServiceFactory.getService("workServiceImpl");
+
+    protected final Log logger = LogFactory.getLog(getClass());
+}
