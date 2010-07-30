c32af7514fa1d65a4f49ff9dd57e060019203cfb
mdshannan <mdshannan@afb23650-9424-0410-ac3c-bbcb0118fe35>
2010-07-30 03:48:05 +0000

    Copied remotely

---
+++ trunk/CCPortlets/CyberCafePortlets/src/java/com/bia/ccm/util/ServiceFactory.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bia.ccm.util;
+
+import com.bia.ccm.services.AdminService;
+import java.util.List;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ *
+ * @author intesar
+ */
+public class ServiceFactory {
+
+    public ServiceFactory() {
+        ApplicationContext context = new ClassPathXmlApplicationContext(
+                new String[]{
+                    "classpath:context/ApplicationContext-Dao.xml",
+                    "classpath:context/ApplicationContext-Service.xml",
+                    "classpath:context/ApplicationContext-Transactions.xml"
+                });
+
+// an ApplicationContext is also a BeanFactory (via inheritance)
+        factory = (BeanFactory) context;
+    }
+    private BeanFactory factory = null;
+    private static ServiceFactory serviceFactory = new ServiceFactory();
+
+    /**
+     * 
+     * @param name userServiceImpl, contactServiceImpl, emailsServiceImpl
+     * @return
+     */
+    public static Object getService(String name) {
+        return serviceFactory.factory.getBean(name);
+    }
+
+    public static void main(String[] args) {
+        ApplicationContext c = new ClassPathXmlApplicationContext(
+                new String[]{
+                    "classpath:context/ApplicationContext-Acegi.xml",
+                    "classpath:context/ApplicationContext-AjaxService.xml",
+                    "classpath:context/ApplicationContext.xml",
+                    "classpath:context/remoting-servlet.xml",
+                    "classpath:context/ApplicationContext-Dao.xml",
+                    "classpath:context/ApplicationContext-Service.xml",
+                    "classpath:context/ApplicationContext-Transactions.xml",
+                    "classpath:context/ApplicationContext-Schedule.xml",
+                });
+
+        BeanFactory f = (BeanFactory) c;
+        AdminService as = (AdminService) serviceFactory.factory.getBean("adminServiceImpl");
+        List list = as.getAllSystems("mdshannan@gmail.com");
+        System.out.println(list.size());
+    }
+}
