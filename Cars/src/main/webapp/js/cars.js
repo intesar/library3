4e041eb54c9f07e34e15383f14a18a8b7787543f
mdshannan <mdshannan@afb23650-9424-0410-ac3c-bbcb0118fe35>
2010-12-09 06:42:32 +0000

    updates

---
+++ Cars/src/main/webapp/js/cars.js
@@ -0,0 +1,30 @@
+var priceLimit = 0;
+var mileage = 0;
+var start = 0;
+var max = 20;
+jQuery(document).ready(function() {
+    AjaxCarService.search(priceLimit, mileage, start, max, displayCars);
+});
+var displayCars = function(cars) {
+    var html = "";
+
+    for ( var i=0; i < cars.list.length; i++) {
+        html +=
+        "<div id='car-link'>\n\
+                        <a href='JavaScript:void(0)' class='car-record car'"+cars.list[i].id +">\n\
+                            <table width='600px'>" + cars.list[i].year +
+        " " + cars.list[i].make + " " + cars.list[i].model + " " + cars.list[i].mileage +
+        " $" + cars.list[i].askingPrice + " \n\
+                            </table>\n\
+                        </a> \n\
+                    </div><br/>" +
+        " <img src=''   >"+
+        "  " +cars.list[i].comments + " "
+        +"<br/><br/>"
+        +"<a href='http://localhost:8080/web/cars/library?p_p_id=31&p_p_lifecycle=0&p_p_state=pop_up&p_p_mode=view&p_p_col_id=column-1&p_p_col_count=1&_31_struts_action=%2Fimage_gallery%2Fview_slide_show&_31_folderId=" + cars.photosFolderId + ">Images</a>"
+        + "<a href='#'> Video <a>"
+
+    ;
+    }
+    jQuery("#cars").html(html);
+}
